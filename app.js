e92875989e551645f514ba48af00de35e6e1e617
M Idrus Salam <idrussalam95@gmail.com>
2019-05-02 01:12:27 +0700

    Deployment

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const topicRoutes = require('./routes/topic').route;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
+app.get('/test', (req, res) => {
   res.send('Welcome to Reddit API!');
 });
 
